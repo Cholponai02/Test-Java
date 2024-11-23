048973a6f04c7fe5538ef381e02d6803f94f0c2c
Cholponai Manapova <cholponay112@gmail.com>
2024-11-24 01:38:49 +0600

    updated gggg

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {// neqw
         Scanner scan = new Scanner(System.in);
   /*      System.out.println("Введите фамилию: ");
         String lastName = scan.nextLine();
